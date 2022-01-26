5e35ad15823723eb9acb736e92ea999f659c8186
Aleksey Kurchavov <kurchavov.aleksey@mail.ru>
2022-01-26 21:27:24 +0300

    Add sin, cos, tg, ctg, sqrt, sqr operations

--- app/src/main/java/com/example/kurchavov204/lab01calculator/MainActivity.kt
+++ app/src/main/java/com/example/kurchavov204/lab01calculator/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.NumberFormatException
+import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,44 +24,69 @@ class MainActivity : AppCompatActivity() {
         fieldB = findViewById(R.id.editTextB)
         resultView = findViewById(R.id.textViewResult)
 
-        val onclickListener = View.OnClickListener { view ->
-            var a: Float
-            var b: Float
+        val twoOperandsHandler = View.OnClickListener { view ->
             var result: Float? = null
 
             try {
-                 a = fieldA?.text.toString().toFloat()
-                 b = fieldB?.text.toString().toFloat()
+                val a = fieldA?.text.toString().toFloat()
+                val b = fieldB?.text.toString().toFloat()
 
-             } catch (e: NumberFormatException) {
-                 this.resultView?.text = getString(R.string.error_text)
-                 return@OnClickListener
-             }
-
-            when(view.id) {
-                R.id.buttonAdd -> result = a + b
-                R.id.buttonSubtract -> result = a - b
-                R.id.buttonMultiply -> result = a * b
-                R.id.buttonDivide ->  {
-                    if (b != 0f) {
-                        result = a / b
-                    }
+                when (view.id) {
+                    R.id.buttonAdd -> result = a + b
+                    R.id.buttonSubtract -> result = a - b
+                    R.id.buttonMultiply -> result = a * b
+                    R.id.buttonDivide -> if (b != 0f) result = a / b
                 }
+
+                setResult(result.toString())
+
+            } catch (e: NumberFormatException) {
+                setResult(getString(R.string.error_text))
             }
-
-            if (result == null) {
-                resultView?.text = getString(R.string.error_text)
-                return@OnClickListener
-            }
-
-            resultView?.text = result.toString()
-
         }
 
-        findViewById<Button>(R.id.buttonAdd).setOnClickListener(onclickListener)
-        findViewById<Button>(R.id.buttonSubtract).setOnClickListener(onclickListener)
-        findViewById<Button>(R.id.buttonMultiply).setOnClickListener(onclickListener)
-        findViewById<Button>(R.id.buttonDivide).setOnClickListener(onclickListener)
+        val oneOperandHandler = View.OnClickListener { view ->
+            var result: Float? = null
 
+            try {
+                val a = fieldA?.text.toString().toFloat()
+
+                when (view.id) {
+                    R.id.buttonSinA -> if (-1 <= a && a <= 1) result = kotlin.math.sin(a)
+                    R.id.buttonCosA -> if (-1 <= a && a <= 1) result = kotlin.math.cos(a)
+                    R.id.buttonSqrtA -> if (a >= 0) result = kotlin.math.sqrt(a)
+                    R.id.buttonSqrA -> result = a.pow(2)
+                    R.id.buttonTgA -> result = kotlin.math.tan(a)
+                    R.id.buttonCtgA -> result = 1 / kotlin.math.tan(a)
+                }
+
+                setResult(result.toString())
+
+            } catch (e: NumberFormatException) {
+                setResult(getString(R.string.error_text))
+            }
+        }
+
+        findViewById<Button>(R.id.buttonAdd).setOnClickListener(twoOperandsHandler)
+        findViewById<Button>(R.id.buttonSubtract).setOnClickListener(twoOperandsHandler)
+        findViewById<Button>(R.id.buttonMultiply).setOnClickListener(twoOperandsHandler)
+        findViewById<Button>(R.id.buttonDivide).setOnClickListener(twoOperandsHandler)
+
+        findViewById<Button>(R.id.buttonSinA).setOnClickListener(oneOperandHandler)
+        findViewById<Button>(R.id.buttonCosA).setOnClickListener(oneOperandHandler)
+
+        findViewById<Button>(R.id.buttonSqrtA).setOnClickListener(oneOperandHandler)
+        findViewById<Button>(R.id.buttonSqrA).setOnClickListener(oneOperandHandler)
+        findViewById<Button>(R.id.buttonTgA).setOnClickListener(oneOperandHandler)
+        findViewById<Button>(R.id.buttonCtgA).setOnClickListener(oneOperandHandler)
+    }
+
+    private fun setResult(resultTxt: String?) {
+        if (resultTxt == null || resultTxt == "null") {
+            setResult(getString(R.string.error_text))
+
+        } else {
+            resultView?.text = resultTxt
+        }
     }
 }
